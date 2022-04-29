974c783d9f64722d9bff00b8f01e03137f9b1b2f
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-04-29 22:39:05 +0600

    load data from mongoDb

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,16 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const useProducts = () =>{
-    
-    const [products, setProducts] = useState([]);
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
 
-    useEffect( () =>{
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/product")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    return [products, setProducts];
-}
+  return [products, setProducts];
+};
 
 export default useProducts;
